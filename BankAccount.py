4a445af61610a306e32a3457468bbb0d7974b059
SchoolboyPretzel <rolandpelzel@go.rmc.edu>
2025-10-06 08:33:09 -0400

    Update BankAccount.py
    Added some mutator methods

--- BankAccount.py
+++ BankAccount.py
@@ -11,10 +11,10 @@ from transaction import Transaction
 
 class BankAccount:
     OVERDRAFT_FEE = 20.00
-    INTEREST_RATE = 0.075     # interest as a decimal 
+    INTEREST_RATE = 0.075
     _nextAccountNumber = 1000
 
-    def __init__(self, first:str = "", last:str ="", balance:float = 0.0):
+    def __init__(self, first: str = "", last: str ="", balance: float = 0.0):
         assert 1 <= len(first) <= 25 and first.isalpha and isinstance(first, str), 'invalid first name'
         assert 1 <= len(last) <= 40 and last.isalpha and isinstance(last, str), 'invalid last name'
         assert isinstance(balance,float),'invalid balance'
@@ -26,51 +26,63 @@ class BankAccount:
         self._transactions = []
         self._timesOverdrawn = 0
 
-        # increment the next account number 
+        # Increment the next account number. 
         BankAccount._nextAccountNumber += 1
+
+    ### QUERIES ###
     
     # getFirst returns the account's first name 
     # @return: the account's first name
-    def getFirst(self) :
+    def getFirst(self):
         return self._first
     
     # getLast returns the account's last name 
     # @return: the account's last name
-    def getLast(self) :
+    def getLast(self):
         return self._last
     
     # getBalance returns the account's balance 
     # @return: the account's balance
-    def getBalance(self) :
+    def getBalance(self):
         return self._balance
     
     # getAccountNumber returns the account's account number 
     # @return: the account's accunt number
-    def getAccountNumber(self) :
+    def getAccountNumber(self):
         return self._accountNumber
     
     # getTransactions returns the account's transactions list 
     # @return: the account's transaction list
-    def getTransactions(self) :
+    def getTransactions(self):
         return self._transactions
     
     # getTimesOverdrawn returns the account's amount of times overdrawn 
     # @return: the account's amount of times overdrawn
-    def getTimesOverdrawn(self) :
+    def getTimesOverdrawn(self):
         return self._timesOverdrawn
+
+
+    ### COMMANDS ###
     
     def deposit(self,amount:float):
         assert isinstance(amount,float) and amount >= 0.0, 'invalid deposit amount'
-        self._transactions.append(Transaction(len(self._transactions)+1,'deposit',amount))
+        self._transactions.append(Transaction(len(self._transactions)+1, 'deposit', amount))
         self._balance += amount
     
-    def interest(self):
+    def addInterest(self):
         interest = self._balance * BankAccount._interest
-        self._transactions.append(Transaction(len(self._transactions)+1,'interest',interest))
+        self._transactions.append(Transaction(len(self._transactions)+1, 'interest', interest))
         self._balance += interest
 
+    def withdraw(self, amount: float):
+        pass
 
-print(BankAccount._nextAccountNumber)
-account = BankAccount('joe','shmoe')
-print(BankAccount._nextAccountNumber)
+    def transfer(self, other, amount: float):
+        pass
+
+
+if __name__ == "__main__":
+    print(BankAccount._nextAccountNumber)
+    account = BankAccount('joe','shmoe')
+    print(BankAccount._nextAccountNumber)
 
