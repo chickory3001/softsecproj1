1444362da75599b407e1dd5084fe23bf197443a6
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-10-08 08:28:49 -0400

    Update BankAccountTester.py

--- BankAccountTester.py
+++ BankAccountTester.py
@@ -1,1 +1,23 @@
-# to be implemented
+#Tester for bankAccountClass
+import unitttest
+from bankAccountTester import BankAccount
+class TestBankAccount(unittest.TestCase):
+    
+    
+    first = input("Enter First Name:")
+    last = input("Enter Last Name:")
+    balance = float(input("Enter the amount of money being deposited:"))
+    bankaccount(first, last, balance)
+    
+    #------------------------------------------------------------------------------------------------------------
+    #This is where the information returns to: First and Last Name, Bank Account Number, Balance
+    BankAccount.printAccount()
+    BankAccount.printTransactions()
+    #------------------------------------------------------------------------------------------------------------
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    unittest.main()
