3146406ab81e41b3ae5577d9aade95fcc44dc635
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-10-29 08:57:05 -0400

    Update CheckingAccount.py

--- CheckingAccount.py
+++ CheckingAccount.py
@@ -1,6 +1,6 @@
 #This is the creation of the checking account
 from client import ClientNumber
-checkingInterst = .0015
+checkingInterest = .0015
 class CheckingAccount:
   def __init__(self, accountNumber):
     pass
