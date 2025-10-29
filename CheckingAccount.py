1cf04a2fa61555784fb5cca9cb945937b2e24590
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-10-29 08:43:06 -0400

    Update CheckingAccount.py

--- CheckingAccount.py
+++ CheckingAccount.py
@@ -3,4 +3,18 @@ from client import ClientNumber
 checkingInterst = .0015
 class CheckingAccount:
   def __init__(self, accountNumber):
+    pass
+  
+  #deposit into the checking account
+  def deposit(self):
+    pass
+  #withdraw from checking account
+  def withdraw(self):
+    pass
+  #returns the calculating interest
+  def calculateInterest(self):
+    pass
+  #transfers from checking into savings
+  def transfer(self):
+    pass
     
