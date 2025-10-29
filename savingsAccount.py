f6d96f20c1ae40c707c805b722587edf6f48ffb1
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-10-29 08:01:32 -0400

    Update savingsAccount.py

--- savingsAccount.py
+++ savingsAccount.py
@@ -1,6 +1,18 @@
 #This creates the savings account constants
 from Client import client
+interest = .0045
 class SavingsAccount:
   def __init__(self):
-    accountNumber = client_
+    accountNumber = client_clientNumber
+
+  def calculateInterest(self):
+    pass
+  
+  def deposit(self):
+    pass
+  
+  def display(self):
+    pass
+    
+    
     
