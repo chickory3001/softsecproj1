12b5c06fa3e15adfeca6d4b9ea18c3beb7d1be4c
Henry <razermade91@gmail.com>
2025-10-30 18:51:04 -0400

    update

--- AddressTester.py
+++ AddressTester.py
@@ -3,16 +3,58 @@ Simple Unit Test using Python's unittest module and assertions
 
 Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
 
-Import the unittest module and the BankAccount module.
+Import the unittest module and the Address module.
 Test each method with at least one unit test.
 """
 
 import unittest
-from BankAccount import BankAccount
-"""Define TestBankAccount class by extending the unittest.TestCase class"""
+from Address import Address
 
 class TestAddress(unittest.TestCase):
+    STREET = 'timmydrive'
+    CITY = 'glenallen'
+    STATE = 'VA'
+    #setup creates an address
     def setUp(self):
         # print('setup')
-        self.bankAccount1 = BankAccount("Tim", "Apple")
-        self.bankAccount2 = BankAccount("Billiam", "Gates")
+        self.address1 = Address(TestAddress.STREET,TestAddress.CITY,TestAddress.STATE)
+    
+    #tests constructor
+    def test_constructor(self):
+        self.assertEqual(self.address1._street,TestAddress.STREET)
+        self.assertEqual(self.address1._city,TestAddress.CITY)
+        self.assertEqual(self.address1._state,TestAddress.STATE)
+
+    #tests constructor assertions 
+    def test_constructor_asserts(self):
+        #all of these are expected to throw asserts, and be caught by the with: line 
+        #test string type assert
+        with self.assertRaises(AssertionError):
+            Address(1,TestAddress.CITY,TestAddress.STATE)
+        with self.assertRaises(AssertionError):
+            Address(TestAddress.STREET,1,TestAddress.STATE)
+        with self.assertRaises(AssertionError):
+            Address(TestAddress.STREET,TestAddress.CITY,1)
+        #test alphanumeric assert 
+        with self.assertRaises(AssertionError):
+            Address(' @#$%^',TestAddress.CITY,TestAddress.STATE)
+        with self.assertRaises(AssertionError):
+            Address(TestAddress.STREET,' @#$%^',TestAddress.STATE)
+        with self.assertRaises(AssertionError):
+            Address(TestAddress.STREET,TestAddress.CITY,' @#$%^')
+        #test length assert
+        with self.assertRaises(AssertionError):
+            Address('',TestAddress.CITY,TestAddress.STATE)
+        with self.assertRaises(AssertionError):
+            Address('a'*31,TestAddress.CITY,TestAddress.STATE)
+        with self.assertRaises(AssertionError):
+            Address(TestAddress.STREET,'',TestAddress.STATE)
+        with self.assertRaises(AssertionError):
+            Address(TestAddress.STREET,'a'*31,TestAddress.STATE)
+        with self.assertRaises(AssertionError):
+            Address(TestAddress.STREET,TestAddress.CITY,'aaaa')
+        # test in list of states assert
+        with self.assertRaises(AssertionError):
+            Address(TestAddress.STREET,TestAddress.CITY,'ZZ')
+        with self.assertRaises(AssertionError):
+            Address(TestAddress.STREET,TestAddress.CITY,'va') 
--- RunAllTests.py
+++ RunAllTests.py
@@ -1,7 +1,7 @@
 #simple program to import all the test classes and run them
 import unittest
 from TestTransaction import TestTransaction
-from BankAccountTester import TestBankAccount
+# from BankAccountTester import TestBankAccount
 from AddressTester import TestAddress
 
 if __name__ == '__main__':
