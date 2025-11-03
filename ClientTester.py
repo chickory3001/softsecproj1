9c8f0572974be35c4aa6c859b245d459ac3b2dff
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-11-03 09:16:35 -0500

    Update ClientTester.py

--- ClientTester.py
+++ ClientTester.py
@@ -1,1 +1,9 @@
+# Client.py
+#
+# Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
+# 
+# CSEC323 - Project 2
+# This is the tester for the client 
+
+from Client import Client
 
