86f5a569306a4c213c6f33b4ea6284f066ad70b5
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-11-04 11:13:58 -0500

    Update ClientTester.py

--- ClientTester.py
+++ ClientTester.py
@@ -8,13 +8,22 @@
 import unittest
 from Address import Address
 from Client import Client
+
+
 class ClientTester:
-  def __init__(unittest.TestCase):
-    firstName = [reggie, homer, jeff, matt, ryan]
-    lastName = [sonic, randall, arin, dan, adam]
-    phoneNumber = [1111111111, 2222222, 33333333, 44444444, 55555555, 66666666]
-    address = []
-    
+#Testers
+  firstNames = ["Reggie", "Homer", "Jeff", "Matt", "Ryan"]
+  lastNames = ["Sonic", "Randall", "Arin", "Dan", "Adam"]
+  phoneNumbers = ["3456789012", "4567890123", "5678901234", "6789012345", "7890123456"]
+  address = Address("CaryStreet", "Richmond", "Va")
+  
+  def testNames(self):
+    for i in range(len(self.firstNames)):
+      c = Client(self.firstNames[i], self.lastNames[i], self.assertEqual(c.getFirstName(), self.firstName[i]), self.assertEqual(c.getLastName(), self.lastNames[i])self.assertEqual(len(c._accounts),1)
+  #---------------------------------------------------------------------------------------------------------------
+  #currently working on this, I don't come home until 10pm today, it will be done. Just don't touch please. -Josh
+  #---------------------------------------------------------------------------------------------------------------
+  
     
     
     
