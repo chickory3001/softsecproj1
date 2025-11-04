a7c4307a94802b34b8695e389443a9b1b25c5c8e
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-11-04 10:56:11 -0500

    Update ClientTester.py

--- ClientTester.py
+++ ClientTester.py
@@ -13,6 +13,8 @@ class ClientTester:
     firstName = [reggie, homer, jeff, matt, ryan]
     lastName = [sonic, randall, arin, dan, adam]
     phoneNumber = [1111111111, 2222222, 33333333, 44444444, 55555555, 66666666]
+    address = []
+    
     
     
     
