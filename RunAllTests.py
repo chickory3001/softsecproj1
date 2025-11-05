9bc9635ead0dbc8aad514124a655271008ebcb9f
Joe Mama <razermade91@gmail.com>
2025-11-05 15:07:48 -0500

    update

--- RunAllTests.py
+++ RunAllTests.py
@@ -3,6 +3,7 @@ import unittest
 from TestTransaction import TestTransaction
 from CheckingAccountTester import TestCheckingAccount
 from SavingsAccountTester import TestSavingsAccount
+from ClientTester import TestClient
 from AddressTester import TestAddress
 
 if __name__ == '__main__':
