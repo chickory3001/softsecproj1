930e349776416e3e2d31b7d20180f79d3c7ed83f
SchoolboyPretzel <rolandpelzel@go.rmc.edu>
2025-11-05 19:39:23 -0500

    Update ClientTester.py
    readability

--- ClientTester.py
+++ ClientTester.py
@@ -11,9 +11,10 @@ import unittest
 from Client import Client
 from Address import Address
 
-# class to test the client class 
+# Class to test the client class 
 class TestClient(unittest.TestCase):
-    #Constants
+    
+    # Constants
     FIRSTNAME = 'timmy'
     LASTNAME = 'smith'
     PHONE= "9123456789"
@@ -26,11 +27,11 @@ class TestClient(unittest.TestCase):
     def setUp(self):
         self.client1 = Client(TestClient.FIRSTNAME,TestClient.LASTNAME,TestClient.PHONE,TestClient.ADDRESS, TestClient.INITIALTYPE)
     
-    # teardown resets the first client number 
+    # Teardown resets the first client number 
     def tearDown(self):
         Client._nextClientNumber = 100
     
-    #Tests constuctor   
+    # Tests constuctor   
     def test_constructor(self):
         if TestClient.DEBUG:
             print("\nTesting the constructor")
@@ -46,12 +47,12 @@ class TestClient(unittest.TestCase):
         client2 = Client(TestClient.FIRSTNAME,TestClient.LASTNAME,TestClient.PHONE,TestClient.ADDRESS, 's')
         self.assertEqual(client2._accounts[0]._type, 's')
 
-    # testing assertions in the constructor 
+    # Testing assertions in the constructor 
     def test_constructor_asserts(self):
         if TestClient.DEBUG:
             print("\nTesting the constructor assertions")
         
-        # test first name assert 
+        # Test first name assert 
         with self.assertRaises(AssertionError):
             client = Client(12312321,TestClient.LASTNAME,TestClient.PHONE,TestClient.ADDRESS, TestClient.INITIALTYPE)
         with self.assertRaises(AssertionError):
@@ -60,7 +61,8 @@ class TestClient(unittest.TestCase):
             client = Client('',TestClient.LASTNAME,TestClient.PHONE,TestClient.ADDRESS, TestClient.INITIALTYPE)
         with self.assertRaises(AssertionError):
             client = Client('a'*26,TestClient.LASTNAME,TestClient.PHONE,TestClient.ADDRESS, TestClient.INITIALTYPE)
-        # test last name assert 
+        
+        # Test last name assert 
         with self.assertRaises(AssertionError):
             client = Client(TestClient.FIRSTNAME,12312321,TestClient.PHONE,TestClient.ADDRESS, TestClient.INITIALTYPE)
         with self.assertRaises(AssertionError):
@@ -69,7 +71,8 @@ class TestClient(unittest.TestCase):
             client = Client(TestClient.FIRSTNAME,'',TestClient.PHONE,TestClient.ADDRESS, TestClient.INITIALTYPE)
         with self.assertRaises(AssertionError):
             client = Client(TestClient.FIRSTNAME,'a'*41,TestClient.PHONE,TestClient.ADDRESS, TestClient.INITIALTYPE)
-        # test phone number assert 
+        
+        # Test phone number assert 
         with self.assertRaises(AssertionError):
             client = Client(TestClient.FIRSTNAME,TestClient.LASTNAME,9123456789,TestClient.ADDRESS, TestClient.INITIALTYPE)
         with self.assertRaises(AssertionError):
@@ -78,10 +81,12 @@ class TestClient(unittest.TestCase):
             client = Client(TestClient.FIRSTNAME,TestClient.LASTNAME,'a'*11,TestClient.ADDRESS, TestClient.INITIALTYPE)
         with self.assertRaises(AssertionError):
             client = Client(TestClient.FIRSTNAME,TestClient.LASTNAME,'0123456789',TestClient.ADDRESS, TestClient.INITIALTYPE)
-        # test address assert 
+        
+        # Test address assert 
         with self.assertRaises(AssertionError):
             client = Client(TestClient.FIRSTNAME,TestClient.LASTNAME,TestClient.PHONE,'ssssssss', TestClient.INITIALTYPE)
-        # test inital account type assert 
+        
+        # Test inital account type assert 
         with self.assertRaises(AssertionError):
             client = Client(TestClient.FIRSTNAME,TestClient.LASTNAME,TestClient.PHONE,TestClient.ADDRESS, 123131)
         with self.assertRaises(AssertionError):
@@ -95,24 +100,24 @@ class TestClient(unittest.TestCase):
         self.assertEqual(len(self.client1._accounts), 1)
         self.assertEqual(self.client1._accounts[0]._accountNumber,1000)
 
-        #Testing a valid checking account opening
+        # Testing a valid checking account opening
         self.client1.openAccount("c")
         self.assertEqual(len(self.client1._accounts), 2)
         self.assertEqual(self.client1._accounts[1]._type, "c")
         self.assertEqual(self.client1._accounts[1]._accountNumber,1001)
 
-        #Testing a valid savings account opening
+        # Testing a valid savings account opening
         self.client1.openAccount("s")
         self.assertEqual(len(self.client1._accounts), 3)
         self.assertEqual(self.client1._accounts[2]._type, "s")
         self.assertEqual(self.client1._accounts[2]._accountNumber,1002)
 
-        #Testing that having the type argument not be of type string leads to assert error
+        # Testing that having the type argument not be of type string leads to assert error
         with self.assertRaises(AssertionError):
             self.client1.openAccount(10)
         self.assertEqual(len(self.client1._accounts), 3)
         
-        #Testing that having the type argument not be either "c" or "s" leads to assert error
+        # Testing that having the type argument not be either "c" or "s" leads to assert error
         with self.assertRaises(AssertionError):
             self.client1.openAccount("Invalid")
         self.assertEqual(len(self.client1._accounts), 3)
@@ -121,7 +126,8 @@ class TestClient(unittest.TestCase):
     def test_close_account(self):
         if TestClient.DEBUG:
             print("\nTesting the closeAccount method")
-        #1 is the checking account created from the constructor
+        
+        # 1 is the checking account created from the constructor
         self.assertEqual(len(self.client1._accounts), 1)
 
         # Test assert for trying to close an account with the number not being an int
@@ -136,33 +142,33 @@ class TestClient(unittest.TestCase):
         result = self.client1.closeAccount(9999)
         self.assertEqual(result, False)
 
-        #Adding a 2nd account so we can close it with no balance
+        # Adding a 2nd account so we can close it with no balance
         self.client1.openAccount("c")
         self.assertEqual(len(self.client1._accounts), 2)
 
-        #Closing 2nd account with a balance of zero, should return True
+        # Closing 2nd account with a balance of zero, should return True
         result = self.client1.closeAccount(self.client1._accounts[1].getAccountNumber())
         self.assertEqual(result, True)
         self.assertEqual(len(self.client1._accounts),1)
 
-        #Adding an account so we can close it with a positive balance
+        # Adding an account so we can close it with a positive balance
         self.client1.openAccount("c")
         self.assertEqual(len(self.client1._accounts), 2)
         self.client1._accounts[1].deposit(1000.0)
 
-        #Closing the account with a positive balance, should return True
+        # Closing the account with a positive balance, should return True
         result = self.client1.closeAccount(self.client1._accounts[1].getAccountNumber())
         self.assertEqual(result, True)
         self.assertEqual(len(self.client1._accounts),1)
 
-        #Adding an account so we can close it with a negative balance
+        # Adding an account so we can close it with a negative balance
         self.client1.openAccount("s")
         self.assertEqual(len(self.client1._accounts), 2)
         self.client1._accounts[1].deposit(1)
         self.client1._accounts[1].withdraw(10)
         self.assertTrue(self.client1._accounts[1].getBalance() < 0)
 
-        #Closing the account with a negative balance, should return False
+        # Closing the account with a negative balance, should return False
         result = self.client1.closeAccount(self.client1._accounts[1].getAccountNumber())
         self.assertEqual(result, False)
         self.assertEqual(len(self.client1._accounts),2)
