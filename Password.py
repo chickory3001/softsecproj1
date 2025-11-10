da7be499c8ec27425b75c0c8498d179dce7eb9d0
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-11-10 08:49:26 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -6,6 +6,8 @@ Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
 Creates a password and checks to see if the password is valid
 """
 from ClientHash import Client
+
+#Creates the password object
 class Password:
   def __init__(self):
     self._password = password
