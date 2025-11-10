b40c535c76bb6a105c7ca869a6c6b1b4cbd754bf
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-11-10 08:48:14 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -8,3 +8,7 @@ Creates a password and checks to see if the password is valid
 from ClientHash import Client
 class Password:
   def __init__(self):
+    self._password = password
+  def creationPassword(self):
+    userPassword = scan("Enter a pasword between 8 - 16 characters:")
+    Client(userPassword)
