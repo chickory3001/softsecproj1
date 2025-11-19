3a60ce12c699fb60a9a28681b77610a7038d1a3c
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-11-19 09:20:41 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -7,6 +7,8 @@ Creates a password and checks to see if the password is valid
 """
 from ClientHash import Client
 
+
+invalidPasswordChar = ["/", "\", "<", ">", "|", "_"]
 #Creates the password object
 class Password:
   def __init__(self):
@@ -15,4 +17,12 @@ class Password:
   #Asks user for a password to enter, make it go through the Client Hash written by the professor to check
   def passwordChecker(self):
     userPassword = scan("Enter a pasword between 8 - 16 characters:")
-    Client(userPassword)
+    passwordCheck = invalidPasswordChar[0]
+    while userPassword != "":
+      if userPassword == invalidPasswordChar[passwordCheck]:
+        print("Invalid Character", invalidPasswordChar[passwordCheck])
+      else:
+      userPassword == True
+    
+    
+    
