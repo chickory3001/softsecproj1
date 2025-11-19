f855402761f90492869383dcb656addaf5c05255
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-11-19 16:12:39 -0500

    Update Test_Password.py

--- Test_Password.py
+++ Test_Password.py
@@ -1,1 +1,9 @@
+"""
+Testing the password class
+
+Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
+Import the password and client class
+"""
+from Password import password
+from Client import client
 
