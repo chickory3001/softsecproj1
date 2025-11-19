97bfb77da74d494b0c72e55df7d113748cb8fbf0
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-11-19 16:43:56 -0500

    Update Test_Password.py

--- Test_Password.py
+++ Test_Password.py
@@ -7,7 +7,11 @@ Import the password and client class
 from Password import password
 from Client import client
 
+
+#Created a list of passwords to test, valid tests and invalid test
 class passwordTester(self):
-  self._password = randyBoBandy84$
-  self._invalidPassword = linesAmIRight?||||
+  self._password = ["randyBoBandy84", "Alpha2025", "CodingKing88", "SecurePass12", "NightWolf77"]
+  self._invalidPassword = ["linesAmIRight?||||", "Bad/Pass1", "Wrong\\Key9", "Too<Short", "Space Bar1"] 
+
+
   
