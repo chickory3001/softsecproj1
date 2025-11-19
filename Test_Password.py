ed505678f73d9e005f908e9ed39d3c639e976166
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-11-19 16:14:32 -0500

    Update Test_Password.py

--- Test_Password.py
+++ Test_Password.py
@@ -7,3 +7,7 @@ Import the password and client class
 from Password import password
 from Client import client
 
+class passwordTester(self):
+  self._password = randyBoBandy84$
+  self._invalidPassword = linesAmIRight?||||
+  
