2b60c7652d7c50bf2b2738967e0e8b3e9b62d621
Pr0gress0 <169167195+Pr0gress0@users.noreply.github.com>
2025-11-24 08:56:47 -0500

    Update Name.py
    Added comparison special methods to Name class

--- Name.py
+++ Name.py
@@ -16,4 +16,36 @@ class Name:
     assert isInstance(name, str)
     assert name.isPrintable()
     assert 1 <= len(name)
-      
+
+    self._name = name
+
+  ### QUERIES ###
+
+  # getName returns the name
+  # @return self._name: the name as a string
+  def getName(self) -> str:
+    return self._name
+
+  ### SPECIAL METHODS ###
+    
+  # Tests quality between two names
+  # @ensure self is being compared to another Name
+  # @return: True if equal, else False
+  def __eq__(self, other: 'Name') -> bool:
+    assert isInstance(other, Name), "Invalid comparison"
+    return self._name == other._name
+
+  # Tests less than comparison between two names
+  # @ensure self is being compared to another Name
+  # @return: True if equal, else False
+  def __lt__(self, other: 'Name') -> bool:
+    assert isInstance(other, Name), "Invalid comparison"
+    return self._name < other._name
+
+  # Tests less than or equal to comparison between two names
+  # @ensure self is being compared to another Name
+  # @return: True if equal, else False
+  def __le__(self, other: 'Name') -> bool:
+    assert isInstance(other, Name), "Invalid comparison"
+    return self._name <= other._name
+
