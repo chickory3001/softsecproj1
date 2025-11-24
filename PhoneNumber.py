e25d96b797365dc40d895f6aa4d5f85dadbfc536
SchoolboyPretzel <rolandpelzel@go.rmc.edu>
2025-11-24 08:59:58 -0500

    Update PhoneNumber.py
    Wrote the constructor but it might be incomplete.

--- PhoneNumber.py
+++ PhoneNumber.py
@@ -1,1 +1,32 @@
+# PhoneNumber.py
+# 
+# Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
+# 
+# CSEC323 - Project 3
+# 
+# This module defines the PhoneNumber class.
+# A simple class to hold a phone number.
 
+class PhoneNumber:
+    
+    def __init__(self, phoneNum: str):
+        assert isinstance(phoneNum, str), "phone number must be a string"
+        
+        if phoneNum.isdecimal():
+            assert len(phoneNum) == 10, "phone number length must be 10"
+        
+        # Allow for PhoneNumber class to read phone numbers with hyphens.
+        else:
+            splitNum = phoneNum.split("-")
+            assert len(splitNum) == 3, "improper use of hyphens"
+            
+            phoneNum = ""
+            for nums in splitNum:
+                phoneNum += nums
+            
+            assert len(phoneNum) == 10, "phone number length must be 10"
+            assert phoneNum.isdecimal(), "phone number must be numbers"
+            
+        self._phoneNum = phoneNum
+        
+    
