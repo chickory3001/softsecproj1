e3664210998b3b8d3618ea76b19235a31055f76f
Pr0gress0 <169167195+Pr0gress0@users.noreply.github.com>
2025-11-29 16:20:10 -0500

    Create LastName.py

---
+++ LastName.py
@@ -0,0 +1,14 @@
+# LastName.py
+#
+# Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
+# 
+# CSEC323 - Project 2
+# 
+# This module defines the FirstName class.
+# Holds a single string with restrictions on its structure
+
+class LastName(Name):
+
+  def __init__(self, name: str) -> 'lastName':
+    assert len(name) <= 45
+    super().__init__('s',number)
