34960aa2aa29c9e761a62b733fd7628c648f352d
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-12-02 10:24:25 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -12,50 +12,62 @@ invalidPasswordChar = ["/", "\", "<", ">", "|", "_"]
 #Creates the password object
 class Password:
   def __init__(self):
-    self._password = password
+    self._password = ""
   
   #Sets password, goes throught a while loop to check if the password is valid within the parameters
   #@Require Password being set is valid
   #@Ensure Password is saved to the client's account
-  def passwordChecker(self):
-    self._password = scan("Enter a password between 8 - 16 characters:")  #Ignore this for rn this needs to be in the tester - Josh
-    passwordCheck = invalidPasswordChar[0]
-    
-    #Checks the userPassword with the list of invalid password characters
-    while self._password != "":
-      if self._password == invalidPasswordChar[passwordCheck]:  #Prints why it fails, special characters that can't be used
-        print("Invalid Character", invalidPasswordChar[passwordCheck])
-      elif len(self._password) < 8 and len(self._password) > 16:  #Prints why it fails, doesn't follow length requirements
-        print("Password length isn't valid, needs to be between 8 - 16 characters:")
-      else:
-      self._password == True    #Saves Password
+  def passwordChecker(self, userPassword):
+    for character in invalidPassswordChar:
+      if character in userPassword:
+        print("Invalid character:", character)
+        return False
+
+      if len(userPassword) < 8 or len(userPassword) > 16:
+        print("Password length must be between 8 - 16 characters:")
+        return False
+    return True
 
     
     #Changes the password the user wants to enter
     #@Require Password is already associated with an account
     #@Ensure Password is being changed
     def changePassword(self):
-      userPassword = scan("Enter the previous password")
-      if self._password != userPassword:  #Returns false if the previous password isn't entered correctly
+      oldPass = input("Enter your previous password")  #Check previous password
+      if oldPass != self._password:
+        print("Incorrect Previous Password:")
         return False
-      else:
-        userPassword = scan("Enter your new password betweeen 8 - 16 characters")
-        passwordConfrim = scan("Enter your password again to confirm")
+
+      #Creation of new password and confirm password
+      newPass = input("Enter your new password")
+      confirmPass = input("Enter your new password to confirm")
+      
+      #If the passwords don't match try again
+      while newPass != confirmPass:
+        print("Passwords do not match try again:")
+        newPass = input("Enter your new password")
+        confirmPass = input("Enter your new password to confirm")
+
+      
+      #Checks new password to see if it is valid, if not prompt the user to try again
+      while not self.isValid(newPass):
+        newPass = input("Enter your new password")
+        confirmPass = input("Enter your new password to confirm")
+
+        while newPass != confirmPass:
+        print("Passwords do not match try again:")
+        newPass = input("Enter your new password")
+        confirmPass = input("Enter your new password to confirm")
+
+
+      self._password = newPass
+      print("Password changed successfully")
+      return True
         
-        while (userPassword != passwordConfrim):  #If the new passwords aren't the same, will put the user through a loop until the correct password is entered
-          userPassword = scan("Incorrect Password, try again:")
-          
-          
         
-        else:
-          while userPassword !=:  #Redo the while loop from earlier, to verify the password being changed follows the right parameters
-          if userPassword == invalidPasswordChar[passwordCheck]:
-            print("Invalid Character", invalidPasswordChar[passwordCheck])
-          elif len(userPassword) < 8 and len(userPassword) > 16:
-            print("Password length isn't valid, needs to be between 8 - 16 characters")
-          else:
-            self._password = userPassword  #Resets to the new password
-        
+
+
+      
         
       
       
