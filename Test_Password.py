75efe3898d050e88ee351bb4aef7b6e54af11385
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-12-02 10:59:36 -0500

    Update Test_Password.py

--- Test_Password.py
+++ Test_Password.py
@@ -20,7 +20,33 @@ class passwordTester(self):
     print("Testing valid passwords:")
     for passwords in self.validPasswords:
       result = self.passwordObj.isValid(passwords)
-      print(f" {passwords}: {"Fail (correct)" if not result else "Pass (Incorrect!)"}")
+      print(f" {passwords}: {"FAIL (correct)" if not result else "PASS (should NOT be valid!)"}")
+  
+  #Testing invalid passwords form list
+  def testInvalidPass(self):
+    print("Testing invalid passwords:")
+    for passwords in self.invalidPasswords:
+      result = self.passwordObj.isValid(passwords)
+      print(f"  {passwords}: {'FAIL (correct)' if not result else 'PASS (should NOT be valid!)'}")
+
+  def testChangingPassword(self):
+    print("Testing password changes")
+    oldPass = self._validPassword[0]
+    newPass = "ValidNewPass67"
+
+    self.passwordObj._password = oldPass
+    changeResult = self.passwordObj.changePassword(oldPass, newPass)
+    print(f" Changing VALID -> VALID: {oldPass} -> {newPass}: {'SUCCESS' if changeResult else 'FAIL'}")
+
+    oldInvalid = self._invalidPassword[0]
+    newInvalid = "invalidNewPass|||67"
+
+    self.passwordObj._password = oldInvalid
+    changeResult = self.passwordObj.changePassword(oldInvalid, newInvalid)
+    print(f" Changing INVALID -> INVALID: {oldInvalid} -> {newInvalid}: {SUCCESS (should NOT happen!}' if changeResult else 'FAIL (correct)'}")
+
+
+    
 
 if __name__ == "__main__":
   tester = PasswordTester()
