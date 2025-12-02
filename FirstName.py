93ff44e6a977e40f2552cc1da253657a8e8ee69a
Pr0gress0 <169167195+Pr0gress0@users.noreply.github.com>
2025-12-02 09:44:50 -0500

    Update FirstName.py

--- FirstName.py
+++ FirstName.py
@@ -2,7 +2,7 @@
 #
 # Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
 # 
-# CSEC323 - Project 2
+# CSEC323 - Project 3
 # 
 # This module defines the FirstName class.
 # Holds a single string with restrictions on its structure
@@ -12,6 +12,6 @@ from Name import Name
 class FirstName(Name):
 
   def __init__(self, name: str) -> 'firstName':
+    super().__init__(name)
     assert len(name) <= 25
-    super().__init__('s',number)
 
