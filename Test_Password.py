f07be2f9e0dee818a54376f5777b192295b52910
WimbleBimble64 <ajsajoshgamer@gmail.com>
2025-12-02 10:35:42 -0500

    Update Test_Password.py

--- Test_Password.py
+++ Test_Password.py
@@ -5,13 +5,29 @@ Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
 Import the password and client class
 """
 from Password import password
-from Client import client
+from Client import Client
 
 
 #Created a list of passwords to test, valid tests and invalid test
 class passwordTester(self):
-  self._password = ["randyBoBandy84", "Alpha2025", "CodingKing88", "SecurePass12", "NightWolf77"]
-  self._invalidPassword = ["linesAmIRight?||||", "Bad/Pass1", "Wrong\\Key9", "Too<Short", "Space Bar1"] 
+  def __init__(self):
+    self._validPassword = ["randyBoBandy84", "Alpha2025", "CodingKing88", "SecurePass12", "NightWolf77"]
+    self._invalidPassword = ["linesAmIRight?||||", "Bad/Pass1", "Wrong\\Key9", "Too<Short", "Space Bar1"] 
+    self.passwordObj = Password()  #creating a password object to test
+
+  #Testing valid passwords from list
+  def testValidPass(self):
+    print("Testing valid passwords:")
+    for passwords in self.validPasswords:
+      result = self.passwordObj.isValid(passwords)
+      print(f" {passwords}: {"Fail (correct)" if not result else "Pass (Incorrect!)"}")
+
+if __name__ == "__main__":
+  tester = PasswordTester()
+  tester.test_validPassword()
+  tester.test_invalidPasswords()
+
+
 
 
   
