9c4ad8b368ead4e3a9a098447ea0072331d6bad8
Pr0gress0 <169167195+Pr0gress0@users.noreply.github.com>
2025-12-02 11:22:31 -0500

    Update FirstNameTester.py

--- FirstNameTester.py
+++ FirstNameTester.py
@@ -1,3 +1,12 @@
+# FirstNameTester.py
+#
+# Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
+# 
+# CSEC323 - Project 3
+# 
+# Import the unittest module and the FirstName class.
+# Test each method of both FirstName and Name to get full branch coverage.
+# Note: When using coverage report, it will show the debug statements as failing.
 
 from FirstName import FirstName
 
