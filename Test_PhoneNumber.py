8d86ea5ce153c3c1721b7001be18c80b74bc5657
SchoolboyPretzel <rolandpelzel@go.rmc.edu>
2025-12-05 17:35:40 -0500

    Update Test_PhoneNumber.py
    Test phone number with hyphens

--- Test_PhoneNumber.py
+++ Test_PhoneNumber.py
@@ -14,19 +14,24 @@ from PhoneNumber import PhoneNumber
 class TestPhoneNumber(unittest.TestCase):
 
   VALIDPHONE = '9123456789'
+  ALSO_VALID = "912-345-6789"
   DEBUG = True
 
   def setUp(self):
     self.phonenum = PhoneNumber(TestPhoneNumber.VALIDPHONE)
+    self.phone2 = PhoneNumber(TestPhoneNumber.ALSO_VALID)
 
   def test_constructor(self):
     if self.DEBUG:
       print("\nTesting the constructor")
     
     self.assertEqual(self.phonenum.getPhoneNum(), TestPhoneNumber.VALIDPHONE)
+    self.assertEqual(self.phone2.getPhoneNum(), TestPhoneNumber.VALIDPHONE)
   
   def test_constructor_asserts(self):
-
+    pass
+  
+  
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
