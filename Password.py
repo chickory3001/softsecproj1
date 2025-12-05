97ee022e9ddc3b24daf773fde6edc67506900b4c
Pr0gress0 <169167195+Pr0gress0@users.noreply.github.com>
2025-12-05 09:32:34 -0500

    Update Password.py

--- Password.py
+++ Password.py
@@ -5,9 +5,6 @@ Created by: J. Bodde, C. Burrell, H. Hickory, R. Pelzel, C. Triplett
 
 Creates a password and checks to see if the password is valid, also resets when correct condentials have been fulfilled
 """
-from ClientHash import Client
-
-
 
 class Password:
   invalidPasswordChar = ["/", "\\", "<", ">", "|", "_"]
@@ -63,3 +60,46 @@ class Password:
     self._password = newPass
     print("Password changed successfully")
     return True
+
+
+  # Method to securly hash the password
+    # @parameter: the password to hash
+    # @require: 8 <= len(password) <=  16
+    # @require: password does not contain "/", "\\", "<", ">", "|"
+  def _createSecureHash(self, password):
+      assert isinstance(password, str), "Invalid type"
+      assert Client.PASS_MIN_LEN <= len(password) <=  Client.PASS_MAX_LEN, "Invalid length" 
+      assert _checkSyntax(password)
+        
+      # Create a random 16 byte salt
+      self._salt = os.urandom(16)
+      self._iterations = 100_000
+      self._hash_algo = 'sha256'
+      self._hash = hashlib.pbkdf2_hmac(
+        self._hash_algo,
+        password.encode('utf-8') + Client.PEPPER.encode('utf-8'),  
+        self._salt,
+        self._iterations
+      )  
+    
+    # Private method to check a password against the stored hash
+    # @parameter: password - the string passed in containing the password to check
+    # @require: 8 <= len(password) <=  16
+    # @require: password does not contain "/", "\\", "<", ">", "|"    
+  def _checkPassword(self, password):
+    #Assertions to check password type, length, and syntax
+    assert isinstance(password, str), "Invalid type"
+    assert Client.PASS_MIN_LEN <= len(password) <=  Client.PASS_MAX_LEN, "Invalid length" 
+    assert _checkSyntax(password)
+
+    # Compute the hash from password entered   
+    passswordHash = hashlib.pbkdf2_hmac(
+      self._hash_algo, 
+      password.encode('utf-8') + Client.PEPPER.encode('utf-8'),  
+      self._salt,
+      self._iterations
+      )
+         
+    # Compare the computed hash and the stored hash and return the result
+    return (passswordHash == self._hash)
+
