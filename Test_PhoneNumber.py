919b88b08fc6f60290b20730e698963b1e5bb92a
Joe Mama <razermade91@gmail.com>
2025-12-05 17:28:12 -0500

    update

--- Test_PhoneNumber.py
+++ Test_PhoneNumber.py
@@ -26,46 +26,7 @@ class TestPhoneNumber(unittest.TestCase):
     self.assertEqual(self.phonenum.getPhoneNum(), TestPhoneNumber.VALIDPHONE)
   
   def test_constructor_asserts(self):
-    if self.DEBUG:
-      print("\nTesting the constructor asserts")
-    
-    with self.assertRaises(AssertionError):
-      FirstName(self.invalidName1)
-    with self.assertRaises(AssertionError):
-      FirstName(self.invalidName2)
-    with self.assertRaises(AssertionError):
-      FirstName(self.invalidName3)
-    with self.assertRaises(AssertionError):
-      FirstName(self.invalidFirst)
-    
-  def test_getName(self):
-    if self.DEBUG:
-      print("\nTesting the getter method")
 
-    self.assertEqual(self.FirstName1.getName(), TestPhoneNumber.validFirst1)
-
-    
-  def test_eq(self):
-    if self.DEBUG:
-      print("\nTesting the equal special method")
-      
-    self.assertTrue(self.FirstName1 == self.FirstName1)
-    self.assertFalse(self.FirstName1 == self.FirstName2)
-    
-  def test_lt(self):
-    if self.DEBUG:
-      print("\nTesting the less than special method")
-            
-    self.assertTrue(self.FirstName1 < self.FirstName2)
-    self.assertFalse(self.FirstName1 < self.FirstName1)
-    
-  def test_le(self):
-    if self.DEBUG:
-      print("\nTesting the less than or equal to special method")
-            
-    self.assertTrue(self.FirstName1 <= self.FirstName1)
-    self.assertTrue(self.FirstName1 <= self.FirstName2)
-    self.assertFalse(self.FirstName2 <= self.FirstName1)
 
 if __name__ == "__main__":
     unittest.main()
