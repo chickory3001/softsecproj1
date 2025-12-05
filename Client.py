c88a47c500187f412690d75bf74a887f11083fdf
Joe Mama <razermade91@gmail.com>
2025-12-05 13:40:54 -0500

    update

--- Client.py
+++ Client.py
@@ -151,6 +151,40 @@ class Client:
         # Return False if the account doesn't exist
         return False
     
+    #Changes the password the user wants to enter
+    #@Require Password is already associated with an account
+    #@Ensure Password is being changed
+    def changePassword(self):
+        oldPass = input("Enter your previous password")  #Check previous password
+        if oldPass != self._password:
+            print("Incorrect Previous Password:")
+            return False
+
+        #Creation of new password and confirm password
+        newPass = input("Enter your new password")
+        confirmPass = input("Enter your new password to confirm")
+        
+        #If the passwords don't match try again
+        while newPass != confirmPass:
+            print("Passwords do not match, try again:")
+            newPass = input("Enter your new password")
+            confirmPass = input("Enter your new password to confirm")
+        
+        #Checks new password to see if it is valid, if not prompt the user to try again
+        while not Password.passwordChecker(newPass):
+            print("Password invalid try again:")
+            newPass = input("Enter your new password")
+            confirmPass = input("Enter your new password to confirm")
+
+            while newPass != confirmPass:
+                print("Passwords do not match try again:")
+                newPass = input("Enter your new password")
+                confirmPass = input("Enter your new password to confirm")
+
+
+        self._password = newPass
+        print("Password changed successfully")
+        return True
 
 
 # if __name__ == "__main__":
--- Password.py
+++ Password.py
@@ -11,7 +11,7 @@ class Password:
 
   def __init__(self, password):
     assert isinstance(password, str), "Invalid type"
-    assert passwordChecker(password), "Invalid password"
+    assert self.passwordChecker(password), "Invalid password"
     self._password = password    #Creates the password object
 
   # Method that will return the password
@@ -30,39 +30,4 @@ class Password:
         return False
     return True
     
-  #Changes the password the user wants to enter
-  #@Require Password is already associated with an account
-  #@Ensure Password is being changed
-  def changePassword(self):
-    oldPass = input("Enter your previous password")  #Check previous password
-    if oldPass != self._password:
-      print("Incorrect Previous Password:")
-      return False
-
-    #Creation of new password and confirm password
-    newPass = input("Enter your new password")
-    confirmPass = input("Enter your new password to confirm")
-    
-    #If the passwords don't match try again
-    while newPass != confirmPass:
-      print("Passwords do not match try again:")
-      newPass = input("Enter your new password")
-      confirmPass = input("Enter your new password to confirm")
-
-    
-    #Checks new password to see if it is valid, if not prompt the user to try again
-    while not passwordChecker(newPass):
-      print("Password invalid try again:")
-      newPass = input("Enter your new password")
-      confirmPass = input("Enter your new password to confirm")
-
-      while newPass != confirmPass:
-        print("Passwords do not match try again:")
-        newPass = input("Enter your new password")
-        confirmPass = input("Enter your new password to confirm")
-
-
-    self._password = newPass
-    print("Password changed successfully")
-    return True
 
